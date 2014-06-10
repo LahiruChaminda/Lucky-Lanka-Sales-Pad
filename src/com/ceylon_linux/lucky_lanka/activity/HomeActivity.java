7da1b31838808ec5be5679e71169ad38c4f5c782
Supun Lakshan Wanigarathna Dissanayake <supunlakshan.xfinity@gmail.com>
2014-06-10 23:53:25 +0530

    added LoginActivity and HomeActivity

---
+++ src/com/ceylon_linux/lucky_lanka/activity/HomeActivity.java
@@ -0,0 +1,23 @@
+/*
+ * Intellectual properties of Supun Lakshan Wanigarathna Dissanayake
+ * Copyright (c) 2014, Supun Lakshan Wanigarathna Dissanayake. All rights reserved.
+ * Created on : Jun 10, 2014, 11:22:52 AM
+ */
+package com.ceylon_linux.lucky_lanka.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ceylon_linux.lucky_lanka.R;
+
+/**
+ * @author Supun Lakshan Wanigarathna Dissanayake
+ * @mobile +94711290392
+ * @email supunlakshan.xfinity@gmail.com
+ */
+public class HomeActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_page);
+	}
+}
