e2eb450fb02c6db6639807aad2e5b0f27674adb2
Supun Lakshan Wanigarathna Dissanayake <supunlakshan.xfinity@gmail.com>
2014-06-10 23:48:56 +0530

    Update Database

--- assets/database.sql
+++ assets/database.sql
@@ -1,14 +1,8 @@
 drop table if exists tbl_item;
 drop table if exists tbl_category;
-drop table if exists tbl_customer;
-drop table if exists tbl_driver;
-drop table if exists tbl_distributor;
 drop table if exists tbl_outlet;
 drop table if exists tbl_order_detail;
 drop table if exists tbl_order;
-drop table if exists tbl_rep_location;
-drop table if exists tbl_vehicle;
-drop table if exists tbl_unproductive_call;
 
 create table tbl_category(
 	categoryId integer primary key,
@@ -17,68 +11,35 @@ create table tbl_category(
 create table tbl_item(
 	itemId integer primary key,
 	categoryId integer not null references tbl_category(categoryId) on update cascade,
-	itemCode text not null,
+	itemCode text,
 	itemDescription text check(itemDescription!=''),
-	price real
-);
-create table tbl_customer(
-	customerId integer not null primary key,
-	customerName text not null
-);
-create table tbl_vehicle(
-	vehicleNo text not null primary key
-);
-create table tbl_driver(
-	driverName text not null,
-	driverNIC text not null unique
-);
-create table tbl_distributor(
-	distributorId integer not null primary key,
-	distributorName text not null
+	wholeSalePrice real,
+	retailPrice real,
+	availableQuantity int,
+	loadedQuantity int,
+	freeIssueAvailability int default 0,
 );
 create table tbl_outlet(
 	outletId integer not null primary key,
-	outletName text not null
+	outletName text not null,
+	outletAddress text not null,
+	outletType int not null default 0,
+	outletDiscount real default 0 check (outletDiscount >= 0 && outletDiscount<=100)
 );
 create table tbl_order(
 	orderId integer not null primary key autoincrement,
-	distributorId integer references tbl_distributor(distributorId) on update cascade,
 	outletId integer references tbl_outlet(outletId) on update cascade,
-	customerId integer references tbl_customer(customerId) on update cascade,
 	orderDate long,
-	deliveryDate long,
-	driverName text check(driverName!=''),
-	driverNIC text check(driverNIC!=''),
-	vehicleNo text check(vehicleNo!=''),
 	total real,
 	batteryLevel integer not null,
 	longitude real not null,
-	latitude real not null,
-	type text not null,
-    remarks text
+	latitude real not null
 );
 create table tbl_order_detail(
 	orderId integer not null references tbl_order(orderId) on update cascade on delete cascade,
 	itemId integer not null references tbl_item(itemId) on update cascade,
 	price real not null,
 	discount real,
-	quantity real
-);
-create table tbl_rep_location(
-    repLocationId integer not null primary key autoincrement,
-	repId int not null,
-    longitude real not null,
-	latitude real not null,
-	batteryLevel integer not null,
-    gpsTime long
-);
-create table tbl_unproductive_call(
-    unProductiveCallId integer not null primary key autoincrement,
-    outletId int not null references tbl_outlet(outletId) on update cascade,
-	batteryLevel int not null,
-	repId integer not null,
-	reason real not null,
-	longitude real not null,
-	latitude real not null,
-    time long not null
+	quantity int,
+	freeQuantity int default 0
 );
