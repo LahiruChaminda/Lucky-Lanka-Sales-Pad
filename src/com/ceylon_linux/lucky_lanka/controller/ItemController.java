fcc37784bbc4d1b18128d41d3e5fd6fa4d2cb639
Supun Lakshan Wanigarathna Dissanayake <supunlakshan.xfinity@gmail.com>
2014-06-11 00:07:04 +0530

    Added ItemController

---
+++ src/com/ceylon_linux/lucky_lanka/controller/ItemController.java
@@ -0,0 +1,23 @@
+/*
+ * Intellectual properties of Supun Lakshan Wanigarathna Dissanayake
+ * Copyright (c) 2014, Supun Lakshan Wanigarathna Dissanayake. All rights reserved.
+ * Created on : Jun 10, 2014, 9:42:22 AM
+ */
+package com.ceylon_linux.lucky_lanka.controller;
+
+import android.content.Context;
+
+/**
+ * @author Supun Lakshan Wanigarathna Dissanayake
+ * @mobile +94711290392
+ * @email supunlakshan.xfinity@gmail.com
+ */
+public class ItemController extends AbstractController {
+
+	private ItemController() {
+	}
+
+	public static void DownloadItems(Context context) {
+
+	}
+}
