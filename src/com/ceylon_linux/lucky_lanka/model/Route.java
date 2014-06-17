4082427a6c21feaac69291e94fdd748f8977dc28
Supun Lakshan Wanigarathna Dissanayake <supunlakshan.xfinity@gmail.com>
2014-06-18 00:11:30 +0530

    Added Model Classes

---
+++ src/com/ceylon_linux/lucky_lanka/model/Route.java
@@ -0,0 +1,80 @@
+﻿/*
+ * Intellectual properties of Supun Lakshan Wanigarathna Dissanayake
+ * Copyright (c) 2014, Supun Lakshan Wanigarathna Dissanayake. All rights reserved.
+ * Created on : Jun 13, 2014, 3:46:01 PM
+ */
+
+package com.ceylon_linux.lucky_lanka.model;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * @author Supun Lakshan Wanigarathna Dissanayake
+ * @mobile +94711290392
+ * @email supunlakshan.xfinity@gmail.com
+ */
+public class Route implements Serializable {
+
+	private int routeId;
+	private String routeName;
+	private ArrayList<Outlet> outlets;
+
+	public Route(int routeId, String routeName) {
+		this.routeId = routeId;
+		this.routeName = routeName;
+	}
+
+	public Route(int routeId, String routeName, ArrayList<Outlet> outlets) {
+		this.setRouteId(routeId);
+		this.setRouteName(routeName);
+		this.setOutlets(outlets);
+	}
+
+	public static final Route parseRoute(JSONObject routeJsonInstance) throws JSONException {
+		ArrayList<Outlet> outlets = new ArrayList<Outlet>();
+		JSONArray outletCollection = routeJsonInstance.getJSONArray("outlets");
+		final int OUTLET_COUNT = outletCollection.length();
+		for (int i = 0; i < OUTLET_COUNT; i++) {
+			outlets.add(Outlet.parseOutlet(outletCollection.getJSONObject(i)));
+		}
+		return new Route(
+			routeJsonInstance.getInt("areaId"),
+			routeJsonInstance.getString("area"),
+			outlets
+		);
+	}
+
+	public int getRouteId() {
+		return routeId;
+	}
+
+	public void setRouteId(int routeId) {
+		this.routeId = routeId;
+	}
+
+	public String getRouteName() {
+		return routeName;
+	}
+
+	public void setRouteName(String routeName) {
+		this.routeName = routeName;
+	}
+
+	public ArrayList<Outlet> getOutlets() {
+		return outlets;
+	}
+
+	public void setOutlets(ArrayList<Outlet> outlets) {
+		this.outlets = outlets;
+	}
+
+	@Override
+	public String toString() {
+		return routeName;
+	}
+}
---
+++ src/com/ceylon_linux/lucky_lanka/model/User.java
@@ -0,0 +1,92 @@
+﻿/*
+ * Intellectual properties of Supun Lakshan Wanigarathna Dissanayake
+ * Copyright (c) 2014, Supun Lakshan Wanigarathna Dissanayake. All rights reserved.
+ * Created on : Jun 13, 2014, 11:54:31 AM
+ */
+
+package com.ceylon_linux.lucky_lanka.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Supun Lakshan Wanigarathna Dissanayake
+ * @mobile +94711290392
+ * @email supunlakshan.xfinity@gmail.com
+ */
+public class User implements Serializable {
+
+	private int positionId;
+	private String name;
+	private String address;
+	private String userName;
+	private Long loginTime;
+
+	public User(int positionId) {
+		this.setPositionId(positionId);
+	}
+
+	public User(int positionId, String name, String address, String userName, Long loginTime) {
+		this.setPositionId(positionId);
+		this.setName(name);
+		this.setAddress(address);
+		this.setUserName(userName);
+		this.setLoginTime(loginTime);
+	}
+
+	public static User parseUser(JSONObject userJsonInstance) throws JSONException {
+		if (!userJsonInstance.getBoolean("result")) {
+			return null;
+		}
+		return new User(
+			userJsonInstance.getInt("position_id"),
+			userJsonInstance.getString("name"),
+			userJsonInstance.getString("postal_address"),
+			userJsonInstance.getString("login_name"),
+			new Date().getTime()
+		);
+	}
+
+	public int getPositionId() {
+		return positionId;
+	}
+
+	public void setPositionId(int positionId) {
+		this.positionId = positionId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Long getLoginTime() {
+		return loginTime;
+	}
+
+	public void setLoginTime(Long loginTime) {
+		this.loginTime = loginTime;
+	}
+}
